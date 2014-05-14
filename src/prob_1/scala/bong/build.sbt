c478c371fef8287f95f559e4f19641edd2be9d4a
bongsungju <bongster88@gmail.com>
2014-05-14 06:51:27 +0100

    setting

--- src/prob_1/scala/bong/build.sbt
+++
@@ -1,5 +0,0 @@
-name := "problem1"
-
-version := "1.0"
-
-scalaVersion := "2.10.3"
--- src/prob_1/scala/bong/src/main/scala/prop1.scala
+++
@@ -1,6 +0,0 @@
-object pro1{
-    def main(args: Array[String]){
-        var result = (1 to 1000).filter(p => (p % 3 ==0) || (p % 5 ==0)).sum 
-        println(result);
-    }
-}
